07d634dc30c6c7c99ff745f4681331f331612e46
BORA <a52447879@gmail.com>
2025-02-26 23:05:29 +0900

    feat: loader함수에서 fastify instance에 접근 가능

--- app/src/web/routes/users.tsx
+++ app/src/web/routes/users.tsx
@@ -1,7 +1,8 @@
 import type { Route } from "./+types/users";
 
 export function loader({ context }: Route.LoaderArgs) {
-  context.
+  // loader함수에서 fastify instance에 접근 가능
+  context.app.env.COOKIE_SECRET;
 }
 
 export default function Users() {
