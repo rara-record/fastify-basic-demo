45fddc162e9a960baa6decbcdca37c6f5a481f42
BORA <a52447879@gmail.com>
2025-02-26 23:05:09 +0900

    feat: add loader function for user routes with context type

--- app/src/web/routes/users.tsx
+++ app/src/web/routes/users.tsx
@@ -1,3 +1,9 @@
+import type { Route } from "./+types/users";
+
+export function loader({ context }: Route.LoaderArgs) {
+  context.
+}
+
 export default function Users() {
   return <div>users</div>;
 }
