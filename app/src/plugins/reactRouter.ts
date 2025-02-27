ccad2b41674a0d8ad40bbcd7747bd5f88fde2150
BORA <a52447879@gmail.com>
2025-02-27 09:29:16 +0900

    comment:

--- app/src/plugins/reactRouter.ts
+++ app/src/plugins/reactRouter.ts
@@ -7,6 +7,9 @@ import type {
 } from "fastify";
 import fp from "fastify-plugin";
 
+/**
+ * @mcansh/remix-fastify/react-router를 사용해서 React Router SSR 지원
+ */
 declare module "react-router" {
   interface AppLoadContext {
     app: FastifyInstance;
@@ -17,14 +20,15 @@ declare module "react-router" {
 export default fp(
   async (app) => {
     await app.register(reactRouterFastify, {
-      buildDirectory: "./dist/web",
+      buildDirectory: "./dist/web", // 빌드된 React 애플리케이션을 Fastify에서 서빙
       async getLoadContext(req) {
+        // getLoadContext에서 Fastify 인스턴스와 요청 객체를 React Router의 loader에서 접근 가능하도록 설정
         return { app, req };
       },
     });
   },
   {
     name: "reactRouter",
-    dependencies: ["app.env", "app.gracefulShutdown"],
+    dependencies: ["app.env", "app.gracefulShutdown"], // 필요한 다른 플로그인들이 먼저 로드되도록
   }
 );
