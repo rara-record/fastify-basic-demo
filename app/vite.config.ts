0042c500e9012d0b22e702170f873be4a5fcac2a
BORA <a52447879@gmail.com>
2025-03-04 16:52:43 +0900

    feat: dependencies for improved GraphQL support

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { cjsInterop } from "vite-plugin-cjs-interop";
+import relay from "vite-plugin-relay-lite";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [
+    reactRouter(),
+    relay(),
+    cjsInterop({
+      dependencies: ["react-relay"],
+    }),
+  ],
 });
